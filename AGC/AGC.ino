67161831349df90a4973a492ef35e5d580bead62
Anurag Pendyala <37773369+done-n-dusted@users.noreply.github.com>
2019-02-11 16:06:07 +0530

    arm movement

--- AGC/AGC.ino
+++ AGC/AGC.ino
@@ -1,13 +1,23 @@
+#include<VarSpeedServo.h>
 // brk -> GND
 // dir -> 5v = reverse
 // vcc brk pwm dir gnd
+
+//Stepper motors spec
+    //En High for both setups
+    //stepper front dir 1
+    //back dir 0
+
+//Servo motor
+VarSpeedServo servo;
 
 // base motors
 int motors[] = {8, 9, 10, 11}; //pwm pins
 int dir[] = {40, 41, 42, 43};
 
 //steppers
-int step_delay = 200;
+int arm_step_delay = 800;
+int base_step_delay = 300;
 int base_stepper_pul = 33;
 int base_stepper_ena = 31;
 int base_stepper_dir = 32;
@@ -33,20 +43,30 @@ void setup() {
   digitalWrite(arm_stepper_ena, HIGH);
   digitalWrite(base_stepper_ena, HIGH);
   Serial.begin(9600);
-  
+
+  servo.attach(13);
+  servo.write(0, 100);
+  delay(500);
   for(int i = 0; i < 4; i++){
     pinMode(motors[i], OUTPUT);
     pinMode(dir[i], OUTPUT);
   }
-  delay(2000);
+  delay(2500);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+  picK();
+
+  
+  /*
+  arm_stepper_move(3000, 1);
+  base_stepper_move(3000, 1);
+  
   int spd = 130;
   moveFront(spd);
-  /*
+  
   delay(400);
   moveBack(spd);
   delay(400);
@@ -123,7 +143,7 @@ void moveRight(int s){
   stop();
 }
 
-void turnRight(){
+void turnRight(int s){
   digitalWrite(dir[0], LOW); //bakward
   digitalWrite(dir[1], HIGH);
   digitalWrite(dir[2], LOW);
@@ -137,7 +157,7 @@ void turnRight(){
   stop();
 }
 
-void turnLeft(){
+void turnLeft(int s){
   digitalWrite(dir[0], HIGH); //bakward
   digitalWrite(dir[1], LOW);
   digitalWrite(dir[2], HIGH);
@@ -156,20 +176,20 @@ void base_stepper_move(int steps, int dir){
   for(int i = 0; i < steps; i++)
   {
     digitalWrite(base_stepper_pul,HIGH);
-    delayMicroseconds(step_delay);
+    delayMicroseconds(base_step_delay);
     digitalWrite(base_stepper_pul,LOW);
-    delayMicroseconds(step_delay);
+    delayMicroseconds(base_step_delay);
   }
 }
 void arm_stepper_move(int steps, int dir){
   digitalWrite(arm_stepper_dir,dir);
   for(int i = 0; i < steps; i++)
   {
-    Serial.println(i);
+    //Serial.println(i);
     digitalWrite(arm_stepper_pul,HIGH);
-    delayMicroseconds(step_delay);
+    delayMicroseconds(arm_step_delay);
     digitalWrite(arm_stepper_pul,LOW);
-    delayMicroseconds(step_delay);
+    delayMicroseconds(arm_step_delay);
   }
 }
 void base_stepper_stop(){
@@ -177,4 +197,23 @@ void base_stepper_stop(){
 }
 void arm_stepper_stop(){
   digitalWrite(arm_stepper_pul,HIGH);
+}
+
+
+void picK()
+{
+  servo.write(0, 80);
+  delay(2000);
+  //base_stepper_move(,);
+  base_stepper_stop();
+
+  arm_stepper_move(8000,0);
+  arm_stepper_stop();
+
+  
+  servo.write(45, 10);
+  delay(4000);
+
+  arm_stepper_move(8000,1);
+  arm_stepper_stop();
 }
